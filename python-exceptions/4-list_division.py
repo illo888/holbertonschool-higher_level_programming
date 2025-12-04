88e3662c304786f1d2b5f4fa5a08480cea185643
root <root@ip-10-42-191-77.ec2.internal>
2025-12-04 22:31:50 +0000

    Implement list_division: element-wise division with try/except/finally; handles wrong type, division by 0, out of range; returns list of length list_length

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        res = 0
+        try:
+            a = my_list_1[i]
+            b = my_list_2[i]
+            if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+                print("wrong type")
+            elif b == 0:
+                print("division by 0")
+            else:
+                res = a / b
+        except IndexError:
+            print("out of range")
+        finally:
+            result.append(res)
+    return result
