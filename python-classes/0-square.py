9dc208981940091e1e170bea4eaedff137a69056
root <root@ip-10-42-208-208.ec2.internal>
2025-12-09 11:41:11 +0000

    Add 0-square.py: empty class Square

---
+++ python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that defines an empty Square class.
+
+This module provides a basic Square class as an introduction to
+object-oriented programming in Python. The class is intentionally
+empty to demonstrate the minimal structure of a class definition.
+"""
+
+
+class Square:
+    """Represents a square.
+
+    This is an empty class that serves as a foundation for understanding
+    how classes work in Python. It has no attributes or methods yet,
+    but can still be instantiated to create Square objects.
+    """
+    pass
