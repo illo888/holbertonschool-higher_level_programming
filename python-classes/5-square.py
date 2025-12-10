b3b5f7b29bff41ea11f8ce171ae38c9e126a16c4
root <root@ip-10-42-84-253.ec2.internal>
2025-12-10 07:48:25 +0000

    fix the checker expects you to explicitly reject Boolean values and treat them as not integers.

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -20,7 +20,7 @@ class Square:
     @size.setter
     def size(self, value):
         """Set the size with validation."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
