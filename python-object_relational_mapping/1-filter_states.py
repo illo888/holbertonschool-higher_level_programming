50063f10948c9de491a9e4540cba67e2b0a368de
Shaden Khalid <shadeenn1424@gmail.com>
2026-01-16 23:36:41 +0000

    Fix style

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,23 +1,41 @@
+
 #!/usr/bin/python3
-"""
-Module for filtering states that start with letter N.
-Uses BINARY for case-sensitive matching.
-"""
+
+"""Lists states starting with N from database"""
+
 import MySQLdb
+
 import sys
 
 if __name__ == "__main__":
+
     db = MySQLdb.connect(
+
         host="localhost",
+
         port=3306,
+
         user=sys.argv[1],
+
         passwd=sys.argv[2],
+
         db=sys.argv[3]
+
     )
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+
+    cur.execute(query)
+
     rows = cur.fetchall()
+
     for row in rows:
+
         print(row)
+
     cur.close()
+
     db.close()
+
