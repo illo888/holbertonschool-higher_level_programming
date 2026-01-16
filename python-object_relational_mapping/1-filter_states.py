4c0775c44ed6597224d2791084df23380b82a528
Shaden Khalid <shadeenn1424@gmail.com>
2026-01-16 23:37:45 +0000

    Resolve pycodestyle line length violation in filter module

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,41 +1,24 @@
-
 #!/usr/bin/python3
-
-"""Lists states starting with N from database"""
-
+"""
+Module for filtering states that start with letter N.
+Uses BINARY for case-sensitive matching.
+"""
 import MySQLdb
-
 import sys
 
 if __name__ == "__main__":
-
     db = MySQLdb.connect(
-
         host="localhost",
-
         port=3306,
-
         user=sys.argv[1],
-
         passwd=sys.argv[2],
-
         db=sys.argv[3]
-
     )
-
     cur = db.cursor()
-
     query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
-
     cur.execute(query)
-
     rows = cur.fetchall()
-
     for row in rows:
-
         print(row)
-
     cur.close()
-
     db.close()
-
