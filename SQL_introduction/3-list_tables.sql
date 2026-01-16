71d05baf1a30851284bab83179a7095620e1b177
root <root@ip-10-42-80-176.ec2.internal>
2026-01-16 21:24:30 +0000

    Task 4: create first_table using CREATE TABLE IF NOT EXISTS (id INT, name VARCHAR(256))

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
 -- Lists all the tables of a database
 -- Query to list all tables of a database
 SHOW TABLES;
+=======
+
+-- Script that lists all tables of a given database
+SHOW TABLES;
+
+>>>>>>> e0dc755 (Task 4: create first_table using CREATE TABLE IF NOT EXISTS (id INT, name VARCHAR(256)))
---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,9 @@
+
+-- Script that creates the table `first_table` if it does not already exist
+-- The table has: id INT, name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
